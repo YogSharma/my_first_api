592b5b60a64291b6425c8bf8026444ade4b8099f
Yogsharma <amrajansh2053@gmail.com>
2019-01-14 16:29:28 +0545

    relation created

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Product;
+use App\Review;
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
-    //
+    public function getValue(){
+
+        dd(Review::find(2)->products) ;
+    }
 }

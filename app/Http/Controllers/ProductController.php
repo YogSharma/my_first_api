c07f6511b0b362dbe667f41fd75bb3504e77af5d
Yogsharma <amrajansh2053@gmail.com>
2019-01-14 23:25:08 +0545

    product creation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,18 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use App\Http\Resources\Product\ProductResource;
 use App\Product;
-use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index', 'show');
+    }
+
     public function index(){
         return ProductCollection::collection(Product::paginate(5));
     }
 
     public function show(Product $product){
         return new ProductResource($product);
+    }
+
+    public function store(ProductRequest $request){
+        $product = new Product;
+        $product->name = $request->name;
+        $product->details = $request->description;
+        $product->price = $request->price;
+        $product->stock = $request->stock;
+        $product->discount = $request->discount;
+        $product->save();
+        return response([
+            'data' => new ProductResource($product)
+        ], \Symfony\Component\HttpFoundation\Response::HTTP_CREATED);
     }
 }

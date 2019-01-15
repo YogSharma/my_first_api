0c76897f90d6cfe27c75d9012183ada538622c59
Yogsharma <amrajansh2053@gmail.com>
2019-01-15 12:31:29 +0545

    create review for forduct

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -5,10 +5,20 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ReviewResource;
 use App\Product;
 use App\Review;
+use Symfony\Component\HttpFoundation\Response;
 
 class ReviewController extends Controller
 {
     public function index(Product $productId){
         return ReviewResource::collection($productId->review);
+    }
+
+    public function store(\Illuminate\Http\Request $request,Product $productId){
+        $review = new Review($request->all());
+        $productId->review()->save($review);
+        return response([
+            'data' => new ReviewResource($review),
+            Response::HTTP_CREATED,
+        ]);
     }
 }
